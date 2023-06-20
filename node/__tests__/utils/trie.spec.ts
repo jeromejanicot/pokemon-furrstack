9d0cdbba62dac48eef734b9ab017a195e7078b37
Jerome Janicot <jeromefjanicot@protonmail.com>
2023-06-20 18:00:41 +0200

    trie autosuggest, tests look ok tho they are failling: issues with the deep equality check and the strings?

---
+++ node/__tests__/utils/trie.spec.ts
@@ -0,0 +1,24 @@
+import Trie from "../../src/trie";
+import { describe, it, expect } from "@jest/globals";
+
+describe("Build trie and return autosuggestion", () => {
+  it("Build a Trie from a list of inputs", () => {
+    const inputString = ["pokemon", "pokebowl", "at", "attic", "ants"];
+
+    inputString.map((string) => Trie.insert(string));
+
+    console.log(Trie);
+
+    const invalid = Trie.search("bomb");
+    const autoSuggestion1 = Trie.search("at");
+    const autoSuggestion2 = Trie.search("po");
+    const autoSuggestion3 = Trie.search("a");
+    const complete = Trie.search("pokemon");
+
+    expect(invalid).toEqual(undefined);
+    expect(autoSuggestion1).toEqual(["at", "attic"]);
+    expect(autoSuggestion2).toEqual(["pokemon", "pokebowl"]);
+    expect(autoSuggestion3).toEqual(["at", "attic", "ants"]);
+    expect(complete).toEqual(["pokemon"]);
+  });
+});
--- node/src/trie.ts
+++ node/src/trie.ts
@@ -10,7 +10,7 @@ class Trie {
     this.root = { character: "", word: false, children: [] };
   }
 
-  createNode(char: string) {
+  createNode(char: string): Node {
     return { character: char, word: false, children: [] };
   }
 
@@ -19,11 +19,11 @@ class Trie {
 
     for (let i = 0; i < key.length; i++) {
       const index = key.charCodeAt(i) - 97;
-      if (curr.children != undefined && curr.children[index]) {
-        curr = curr.children[index] as Node;
-        continue;
+      console.log(`charCodeAt: ${key.charCodeAt(i)}`);
+      console.log(`i - 97: ${index}`);
+      if (!curr.children[index]) {
+        curr.children[index] = this.createNode(key.charAt(i));
       }
-      curr.children[index] = this.createNode(key.charAt(i));
       curr = curr.children[index] as Node;
     }
     curr.word = true;
@@ -42,7 +42,7 @@ class Trie {
       prefix += key.charAt(i);
       curr = curr.children[index] as Node;
     }
-    this.suggestRec(curr, rec, prefix);
+    this.suggestRec(curr, rec, prefix.substring(0, prefix.length - 1));
     return rec;
   }
 
@@ -51,12 +51,12 @@ class Trie {
       rec.push(prefix + node.character);
     }
 
-    for (let i = 0; i < node.children.length; i++) {
-      if (node.children[i]) {
-        node = node.children[i] as Node;
-        this.suggestRec(node, rec, prefix + node.character);
-      }
-    }
+    for (const child in node.children)
+      this.suggestRec(
+        node.children[child] as Node,
+        rec,
+        prefix + node.character,
+      );
   }
 }
 
